fcf58742e121171af4be566ada065c08c15f9fd5
minniverse <miny37088@gmail.com>
2025-04-24 19:59:10 +0700

    proweb P6

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('pages.home'); // 'pages' adalah nama folder, 'home' adalah nama file Blade (tanpa .blade.php)
 })->name('home');
+
+Route::get('/produk', [ProductController::class, 'index']);
